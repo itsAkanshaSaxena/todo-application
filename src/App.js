4b12466e09b7ecaa979fa72f2a43f0f64d214e54
real-codemaster <saxenaakanshaa@gmail.com>
2023-10-15 11:11:02 +0530

    initialize boilerplate for react application

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import HomePage from "./components/Homepage";
+import LoginSignupPage from "./components/LoginSignupPage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ProtectedRoute } from "./ProtectedRoute";
+
+import ErrorPage from "./components/ErrorPage";
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={LoginSignupPage} />
+          <ProtectedRoute path="/homepage" component={HomePage} />
+          <Route path="*" component={ErrorPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
